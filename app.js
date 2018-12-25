8bb8ccda11a0246197782db6f4833f2013f6c9ef
Łukasz Jagliński <ljaglinski@o2.pl>
2018-12-25 19:34:22 +0100

    Define UI vars in calculateResults function

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const sub = document.querySelector('#calorie-form').addEventListener('submit', f
     e.preventDefault();
 });
 
+//Calculate daily calorie requirements
+function calculateResults() {
+    //UI vars
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const age = document.querySelector('#age').value;
+    const sex = document.querySelector('#sex').value;
+    const activity = document.querySelector('#activity').value;
+    
+}
