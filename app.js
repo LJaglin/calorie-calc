377b7f8d51b49388fc48b4dcc504bfb78b8c3a64
Łukasz Jagliński <ljaglinski@o2.pl>
2018-12-25 19:07:25 +0100

    Create listener for submit

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello');
+//Listener for submit
+const sub = document.querySelector('#calorie-form').addEventListener('submit', function(e) {
+    calculateResults();
+    e.preventDefault();
+});
+
